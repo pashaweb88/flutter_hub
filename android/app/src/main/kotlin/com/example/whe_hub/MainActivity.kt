3f22acbfec4e1ab3e3e8abd53f6dc833364e74b7
pavelsimple <pavel.simple17@gmail.com>
2021-06-18 18:56:08 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/whe_hub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whe_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_data/girl_scheme.dart
@@ -0,0 +1,12 @@
+class GirlScheme {
+  String name;
+  String id;
+  String posterPath;
+  String bannerPath;
+
+  GirlScheme(
+      {required this.name,
+      required this.id,
+      required this.posterPath,
+      required this.bannerPath});
+}
